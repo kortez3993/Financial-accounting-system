86721a5e91726f0c01a57cee05a0a900bf66cd3d
Roman <roma.mixajlov.60@mail.ru>
2026-05-10 23:06:54 +0300

    Commit develop написание кода - Создание класса User в пакете entity - Создание интерфейса UserRepository в пакете repository

---
+++ backend-java/src/main/java/backendjava/entity/User.java
@@ -0,0 +1,27 @@
+package backendjava.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private OffsetDateTime createdAt;
+}
